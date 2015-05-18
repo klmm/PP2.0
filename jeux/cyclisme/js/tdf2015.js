a3cf5d52643c273fb52cb13a6097d62289ea3d25
klmm <kevin.monnery@orange.fr>
2015-05-18 23:09:29 +0200

    18-05
    le html rend pas grand chose pour l'instant, il y a aucun css ;)

--- jeux/cyclisme/js/tdf2015.js
+++ jeux/cyclisme/js/tdf2015.js
@@ -5,15 +5,25 @@ function Init_TDF()
 		
 		var $this = $(this);
 		var id = $(this).attr("value");
-		alert(id);
-		var postData = "id_cal=" + id;
+		
+		var postData = "id_cal=" + id + "id_jeu=4";
 		$.ajax(
 		{
-			url : "/lib/sql/getEventById.php",
+			url : "/lib/render/render_calendrier.php",
 			type: "POST",
 			data : postData,
 			success:function(data, textStatus, jqXHR) 
 			{
+				var result = $.parseJSON(data);
+				var calendrier = result.calendrier;
+				var prono_joueur = result.prono_joueur;
+				var pronos = result.pronos;
+				var cyclistes = result.cyclistes;
+				var equipes = result.equipes;
+				
+				render_pres_panel(calendrier);
+				
+				
 			/*
 				bon ca c'est la grosse fonction ajax !!
 				Suivant ce que je veux faire ...ça va faire mal ^^
@@ -82,5 +92,88 @@ function Init_TDF()
 		var id = $(this).attr("value");
 		//alert(id);
 		//juste affichage de l'image...à voir
+	});
+	
+	$(document).on('click', '#scores > tr', function(e)
+	{
+		var $this = $(this);
+		var joueur = $(this).find(".table-name").html().valueOf();
+		var id_jeu = 4;
+		var id_cal = $(this).parent().attr("id");
+	
+		var postData = "id_cal=" + id_cal + "id_jeu=" + id_jeu + "joueur=" + joueur;
+		$.ajax(
+		{
+			url : "/lib/render/render_prono.php",
+			type: "POST",
+			data : postData,
+			success:function(data, textStatus, jqXHR) 
+			{
+				var result = $.parseJSON(data);
+				var calendrier = result.calendrier;
+				var pronos = result.pronos;
+				var cyclistes = result.cyclistes;
+				var equipes = result.equipes;
+				
+			
+				
+			},
+			error: function(jqXHR, textStatus, errorThrown) 
+			{
+			    // rien
+			}
+		});
+		e.preventDefault(); //STOP default action
+	});
+}
+
+function render_pres_panel(calendrier){
+	
+}
+
+function Build_Chart(el, result){
+	
+	var width = 960,
+		height = 500,
+		radius = Math.min(width, height) / 2;
+
+	var color = d3.scale.ordinal()
+		.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+	var arc = d3.svg.arc()
+		.outerRadius(radius - 10)
+		.innerRadius(0);
+
+	var pie = d3.layout.pie()
+		.sort(null)
+		.value(function(d) { return d.population; });
+
+	var svg = d3.select("body").append("svg")
+		.attr("width", width)
+		.attr("height", height)
+	  .append("g")
+		.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+	d3.csv("data.csv", function(error, data) {
+
+	  data.forEach(function(d) {
+		d.population = +d.population;
+	  });
+
+	  var g = svg.selectAll(".arc")
+		  .data(pie(data))
+		.enter().append("g")
+		  .attr("class", "arc");
+
+	  g.append("path")
+		  .attr("d", arc)
+		  .style("fill", function(d) { return color(d.data.age); });
+
+	  g.append("text")
+		  .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+		  .attr("dy", ".35em")
+		  .style("text-anchor", "middle")
+		  .text(function(d) { return d.data.age; });
+
 	});
 }

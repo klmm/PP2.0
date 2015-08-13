53a057f87b5bc46964f71285445a4e572a8a4aed
Tomimito <thomas.cerato@gmail.com>
2015-08-13 23:08:21 +0200

    refresh auto

--- jeux/cyclisme/2015/vuelta/index.php
+++ jeux/cyclisme/2015/vuelta/index.php
@@ -579,6 +579,22 @@
         <script src="/bower_components/angular/angular.js"></script>
 	<script src="/js/social-buttons.js"></script>
 	
+	<script>
+	    var time = new Date().getTime();
+	    $(document.body).bind("mousemove keypress", function(e) {
+		time = new Date().getTime();
+	    });
+
+	    function refresh() {
+		if(new Date().getTime() - time >= 600000)
+		    window.location.reload(true);
+		else
+		    setTimeout(refresh, 10000);
+	    }
+
+	    setTimeout(refresh, 10000);
+	</script>
+
 	
         <script>
 	    

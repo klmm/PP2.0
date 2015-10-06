c6ae1bf264cc32c1a471d1cddbb5898092e1d6da
Tomimito <thomas.cerato@gmail.com>
2015-10-06 23:11:08 +0200

    inscriptions joueurs avec filtre

--- lib/sql/get_inscriptions.php
+++ lib/sql/get_inscriptions.php
@@ -22,4 +22,34 @@
 	    return 0;
 	}
     }
+    
+    function get_joueur_inscription($id_jeu, $joueur){
+	// On �tablit la connexion avec la base de donn�es
+	require_once($_SERVER['DOCUMENT_ROOT'] . '/admin/titi.php');
+	$bdd = new Connexion();
+	$db = $bdd->getDB();
+
+	//On fait la requete sur le login
+	$sql = "SELECT * FROM joueurs_inscriptions WHERE (id_jeu=? AND joueur=?)";
+	$prep = $db->prepare($sql);
+	$prep->bindValue(1,$id_jeu,PDO::PARAM_INT);
+	$prep->bindValue(2,$joueur,PDO::PARAM_STR);
+	$prep->setFetchMode(PDO::FETCH_OBJ);
+	$prep->execute();
+	
+	$enr = $prep->fetch();
+	
+	if ($enr){
+	    $arr['id_joueurs_inscriptions'] = $enr->id_joueurs_inscriptions;
+	    $arr['id_jeu'] = $enr->id_joueurs_inscriptions;
+	    $arr['joueur'] = $enr->id_joueurs_inscriptions;
+	    $arr['classement'] = $enr->id_joueurs_inscriptions;
+	    $arr['no_mail'] = $enr->id_joueurs_inscriptions;
+	    $arr['filtre'] = $enr->filtre;
+	    return $arr;
+	}
+	else{
+	    return null;
+	}
+    }
 ?>
